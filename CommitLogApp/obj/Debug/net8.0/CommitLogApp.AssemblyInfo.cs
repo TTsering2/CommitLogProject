0b64980c83db1693bfc6ebe7a2758b9103512674
Tsering Tsering <dtseringt2@gmail.com>
2024-04-08 04:28:53 -0400

    minor checks to see if build scripts are running

--- CommitLogApp/obj/Debug/net8.0/CommitLogApp.AssemblyInfo.cs
+++ CommitLogApp/obj/Debug/net8.0/CommitLogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a670688008a55b69ccc615aabe3b67407bb76c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3f3d72b50ead5efe9a1e7385cba6b1914bd7b8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
