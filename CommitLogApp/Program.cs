0a9b322f6c8fd8150f897b81365cfb0a7a2ffc96
Tsering Tsering <dtseringt2@gmail.com>
2024-04-08 05:57:15 -0400

    initial menu and user credential information finished

--- CommitLogApp/Program.cs
+++ CommitLogApp/Program.cs
@@ -1,9 +1,51 @@
 ﻿namespace CommitLogApp;
+using System;
+
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    static  void Main(string[] args){
+       
+        int userInput = 0;
+
+        while(userInput != 9){
+            Menu.PrintMenu();
+            // userInput = Menu.UserChoice();
+            Console.WriteLine("Please enter your choice:");
+            string userInputString = Console.ReadLine();  // Read user input as string
+
+            userInput = Menu.UserChoice(userInputString);  // Parse user input into an integer
+
+            switch(userInput){
+                //Prompt user for the credentials
+                case 1: 
+                    Console.WriteLine("1 selected)");
+                    try{
+                        var userCredentials = new UserCredentials();
+                        var (accessToken, username, repository) =  userCredentials.PromptUser();
+                    } catch (ArgumentException ex){
+                        Console.WriteLine($"Input error: {ex.Message}");
+                    } catch (Exception ex) {
+                        Console.WriteLine($"An error has occured: {ex.Message}");
+                    }
+                    break;
+
+                case 2:
+                    Console.WriteLine("2 selected)");
+                    break;
+
+                case 3:
+                    Console.WriteLine("3 selected)");
+                    break;
+
+                case 9:
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice, please enter again!");
+                    break;
+            }
+        }
     }
 }
--- CommitLogApp/obj/Debug/net8.0/CommitLogApp.AssemblyInfo.cs
+++ CommitLogApp/obj/Debug/net8.0/CommitLogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b64980c83db1693bfc6ebe7a2758b9103512674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ba30bce37d745ef2ac0eac68f8066c61e8835c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
