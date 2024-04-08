56a2af2fccfdd228fac8875f6e0c760d634dd049
Tsering Tsering <dtseringt2@gmail.com>
2024-04-08 07:22:21 -0400

    services finished

--- CommitLogApp/Program.cs
+++ CommitLogApp/Program.cs
@@ -1,26 +1,30 @@
 ﻿namespace CommitLogApp;
 using System;
-
+using System.Security.AccessControl;
 
 class Program
 {
-    static  void Main(string[] args){
+    static async void Main(string[] args){
        
         int userInput = 0;
+        string accessToken = null;
+        string username = null;
+        string repository = null;
 
         while(userInput != 9){
             Menu.PrintMenu();
             Console.WriteLine("Please enter your choice:");
             string userInputString = Console.ReadLine();  
             userInput = Menu.UserChoice(userInputString);  
-
+           
             switch(userInput){
                 //Prompt user for the credentials
                 case 1: 
                     Console.WriteLine("1 selected)");
                     try{
                         var userCredentials = new UserCredentials();
-                        var (accessToken, username, repository) =  userCredentials.PromptUser();
+                        (accessToken, username, repository) =  userCredentials.PromptUser();
+
                     } catch (ArgumentException ex){
                         Console.WriteLine($"Input error: {ex.Message}");
                     } catch (Exception ex) {
@@ -29,11 +33,35 @@ class Program
                     break;
 
                 case 2:
-                    Console.WriteLine("2 selected)");
+                    Console.WriteLine("Here are your commits for the last 24 hours.");
+                    var since24HoursAgo = DateTime.Now.AddDays(-1);
+                    var timeNow = DateTime.Now;
+
+                    try{
+                        var commitService = new CommitService(accessToken);
+                        await commitService.DisplayCommits(username, repository, since24HoursAgo, timeNow);
+                    } catch (ArgumentException ex){
+                        Console.WriteLine($"Input error: {ex.Message}");
+                    } catch (Exception ex){
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                    }
+                    Console.ReadLine();
                     break;
 
                 case 3:
-                    Console.WriteLine("3 selected)");
+                    Console.WriteLine("Here are your commits for the last week)");
+                    var since7days = DateTime.Now.AddDays(-1);
+                    timeNow = DateTime.Now;
+
+                    try{
+                        var commitService = new CommitService(accessToken);
+                        await commitService.DisplayCommits(username, repository, since7days, timeNow);
+                    } catch (ArgumentException ex){
+                        Console.WriteLine($"Input error: {ex.Message}");
+                    } catch (Exception ex){
+                        Console.WriteLine($"An error occurred: {ex.Message}");
+                    }
+                    Console.ReadLine();
                     break;
 
                 case 9:
