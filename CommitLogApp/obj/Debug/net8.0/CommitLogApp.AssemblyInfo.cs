c806483e514412b5a320f13c0a8f46c580d78f3c
Tsering Tsering <dtseringt2@gmail.com>
2024-04-08 08:56:13 -0400

    pre final merge commit

--- CommitLogApp/obj/Debug/net8.0/CommitLogApp.AssemblyInfo.cs
+++ CommitLogApp/obj/Debug/net8.0/CommitLogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0363ffd8f36484428af2379f46f962ef6b57cf25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3fa9214bf38ef6545972428bf1820f85387fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommitLogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
